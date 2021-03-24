e85c6fb614dd6681f5a3659facc32473b48f683e
Luna Gagnon Lavoie <raccoon.luna.sama@gmail.com>
2021-03-24 16:33:32 -0400

    enlever le code

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,12 +4,7 @@ import firestore from 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAmtX_Q-C-aU64ZGfDxu90myW6qppWbHLI",
-  authDomain: "cours-react-firebase.firebaseapp.com",
-  projectId: "cours-react-firebase",
-  storageBucket: "cours-react-firebase.appspot.com",
-  messagingSenderId: "752948488573",
-  appId: "1:752948488573:web:6720b2db593f3e96c24414"
+ 
 };
 
 // Initialiser Firebase
